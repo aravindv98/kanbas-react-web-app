2e740053c087844af8aadc812c539fff3d9b7859
Aravind Venkatapathy <arsenal.aravind@gmail.com>
2024-02-22 00:59:07 -0500

    Dashboard changes

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -1,10 +1,33 @@
-import Nav from "../Nav";
+import KanbasNavigation from "./Navigation";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Dashboard from "./Dashboard";
 function Kanbas() {
- return (
-   <div>
-     <Nav/>
-     <h1>Kanbas</h1>
-   </div>
- )
+  return (
+    <div className="" style={{height:'100%'}}>
+    <div className="row" style={{height:'100%'}}>
+      <div className="col-md-1">
+        <KanbasNavigation />
+      </div>
+  
+      <div className="col-md-11">
+        <div>
+          <Routes>
+            <Route path="/" element={<Navigate to="Dashboard" />} />
+            <Route path="Account" element={<h1>Account</h1>} />
+            <Route path="Dashboard" element={<Dashboard />} />
+            <Route path="Courses/:courseId/*" element={<h1>Courses</h1>} />
+            <Route path="Calendar" element={<h1>Calendar</h1>} />
+            <Route path="Index" element={<h1>Index</h1>} />
+            <Route path="History" element={<h1>History</h1>} />
+            <Route path="Studio" element={<h1>Studio</h1>} />
+            <Route path="Help" element={<h1>Help</h1>} />
+          </Routes>
+        </div>
+      </div>
+    </div>
+  </div>
+  
+  );
 }
-export default Kanbas
+
+export default Kanbas;
