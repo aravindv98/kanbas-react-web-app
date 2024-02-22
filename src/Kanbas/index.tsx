19f19b5d16e141c425b2b5d6823af049bd56fd43
Aravind Venkatapathy <arsenal.aravind@gmail.com>
2024-02-21 20:16:37 -0500

    lab final code changes

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -1,13 +1,10 @@
-import {Link} from "react-router-dom";
-
+import Nav from "../Nav";
 function Kanbas() {
- return(
-  <div>
-   <Link to="/Labs/a3">A3</Link> |
-   <Link to="/Kanbas">Kanbas</Link> |
-   <Link to="/hello">Hello</Link> |
-   <h1>Kanbas</h1>
-  </div>
+ return (
+   <div>
+     <Nav/>
+     <h1>Kanbas</h1>
+   </div>
  )
 }
-export default Kanbas;
+export default Kanbas
--- src/Labs/a3/HelloWorld.tsx
+++ src/Labs/a3/HelloWorld.tsx
@@ -1,13 +1,10 @@
-import {Link} from "react-router-dom";
-
+import Nav from "../../Nav";
 function HelloWorld() {
- return(
-  <div>
-   <Link to="/Labs/a3">A3</Link> |
-   <Link to="/Kanbas">Kanbas</Link> |
-   <Link to="/hello">Hello</Link> |
-   <h1>Hello World!</h1>
-  </div>
- )
-};
+ return (
+   <div>
+     <Nav/>
+     <h1>Hello World!</h1>
+   </div>
+ );
+}
 export default HelloWorld;
