08e74dd16e2e9aba30e2fbee0f95f7e8f60c6edc
Aravind Venkatapathy <arsenal.aravind@gmail.com>
2024-04-19 18:56:23 -0400

    dependency changes

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -7,6 +7,7 @@ import store from "./store";
 import { Provider } from "react-redux";
 import axios from "axios";
 import Account from "./Account";
+import React from "react";
 
 function Kanbas() {
     const [courses, setCourses] = useState<any[]>([]);
